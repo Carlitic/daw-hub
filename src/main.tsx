94f8d41cc42a8f4ba68b98aa58e8ade79a8b86a9
Carlitic <carloscastanosblanco@gmail.com>
2025-12-22 21:26:36 +0100

    chore: trigger new deployment with debug log

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('DAW-Hub Loaded: v1.0.1 (Re-launch)')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
