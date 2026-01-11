8e8b80ed92849f5a2745ede6bae4e5d12f55de8b
Carlitic <carloscastanosblanco@gmail.com>
2026-01-11 10:08:08 +0100

    Fix base path in vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/DAW-Hub/",
+  base: "/daw-hub/",
   plugins: [react()],
   resolve: {
     alias: {
